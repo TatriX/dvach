9689efc5bc7761734f06d0816ca65cb5d037e06a
TatriX <tatrics@gmail.com>
2018-12-13 12:26:09 +0100

    feat: Add simple cli args parsing and usage docs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+//! dvach is a simple client cli tool for the 2ch.hk imageboard.
+//!
+//! Usage: dvach [board] [thread]
+//! ```
+//! $ dvach # list boards
+//! $ dvach pr # list threads for the "pr" board
+//! $ dvach pr 1299618 # show selected thread
+//! ```
+
+use structopt::StructOpt;
+
+/// Represent available cli args
+#[derive(StructOpt)]
+struct Cli {
+    /// Board to list
+    board: Option<String>,
+
+    /// Thread to show
+    thread: Option<usize>
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::from_args();
+    println!("{:?} {:?}", args.board, args.thread);
 }
