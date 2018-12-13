528843ea80440a47f8c393daef3ea6d871b29798
TatriX <tatrics@gmail.com>
2018-12-13 12:50:16 +0100

    feat: Add basic parsed args handling with dummy functions

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@
 use structopt::StructOpt;
 
 /// Represent available cli args
-#[derive(StructOpt)]
+#[derive(StructOpt, Debug)]
 struct Cli {
     /// Board to list
     board: Option<String>,
@@ -21,5 +21,29 @@ struct Cli {
 
 fn main() {
     let args = Cli::from_args();
-    println!("{:?} {:?}", args.board, args.thread);
+    println!("Got args: {:#?}", &args);
+
+    // run appropriate action based on cli arguments
+    match (args.board, args.thread) {
+        (None, None) => list_boards(),
+        (Some(board), None) => list_threads(&board),
+        (Some(board), Some(thread)) => list_thread(&board, thread),
+        _ => Cli::clap().print_help().expect("Cannot print help"),
+    }
+}
+
+/// Print all available boards.
+///
+/// This function uses "mobile" API, because there is no "list boards"
+/// functionality in the "json" API. At least I didn't find one.
+fn list_boards() {
+
+}
+
+/// Print all available threads for the board.
+fn list_threads(board: &str) {
+}
+
+/// Print all messages in particular thread.
+fn list_thread(board: &str, thread: usize) {
 }
