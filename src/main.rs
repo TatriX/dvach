9b8f46acd351f9ed648669a6255a682b63b7a1a1
TatriX <tatrics@gmail.com>
2019-01-05 12:55:34 +0100

    feat: Improve tui: add search fields for boards and threads

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,6 @@
 //! $ dvach pr 1299618 # show selected thread
 //! ```
 
-use cursive::event::Key;
-use cursive::traits::*;
-use cursive::view::View;
-use cursive::views::{Dialog, LinearLayout, OnEventView, Panel, SelectView, TextView};
-use cursive::Cursive;
 use env_logger;
 use log::debug;
 use reqwest;
@@ -39,11 +34,13 @@ mod boards;
 mod download;
 mod posts;
 mod threads;
+mod tui;
 
-use self::boards::{get_boards, list_boards};
+use self::boards::list_boards;
 use self::download::download;
-use self::posts::*;
-use self::threads::*;
+use self::posts::list_posts;
+use self::threads::list_threads;
+use self::tui::init_tui;
 
 /// Represent available cli args
 #[derive(StructOpt, Debug)]
@@ -87,76 +84,4 @@ fn main() {
         (Some(board), Some(thread)) => list_posts(&board, thread, args.comment_width),
         _ => Cli::clap().print_help().expect("Cannot print help"),
     }
-}
-
-fn init_tui() {
-    let mut siv = Cursive::default();
-    siv.add_layer(
-        Dialog::around(boards_view())
-            .title("Добро пожаловать. Снова.")
-            .button("Выйти", |s| s.quit()),
-    );
-    siv.run();
-}
-
-fn boards_view() -> impl View {
-    let mut select = SelectView::new()
-        .autojump()
-        .on_submit(|s: &mut Cursive, board: &String| s.add_layer(threads_view(board.to_owned())));
-
-    let mut board_ids = get_boards()
-        .into_iter()
-        .map(|board| board.id)
-        .collect::<Vec<_>>();
-    board_ids.sort();
-    select.add_all_str(board_ids);
-
-    select.scrollable().fixed_size((10, 7))
-}
-
-fn threads_view(board: String) -> impl View {
-    let mut threads = get_threads(&board);
-    threads.sort_by(|a, b| a.id.cmp(&b.id));
-
-    let title = format!("/{}/", &board);
-
-    let select = SelectView::new()
-        .autojump()
-        .on_submit(move |s: &mut Cursive, thread: &String| {
-            s.add_layer(posts_view(
-                board.clone(),
-                thread.parse().expect("Cannot parse post id"),
-            ))
-        })
-        .with_all(threads.into_iter().map(|board| {
-            (
-                format!("{} {}", board.id, parse_thread_comment(&board.comment)),
-                board.id,
-            )
-        }));
-
-    Dialog::around(
-        OnEventView::new(select.scrollable()).on_event(Key::Esc, |s| {
-            s.pop_layer();
-        }),
-    )
-    .title(title)
-    .button("Выйти", |s| s.quit())
-}
-
-fn posts_view(board: String, thread: usize) -> impl View {
-    let mut layout = LinearLayout::vertical();
-    for post in get_posts(&board, thread) {
-        layout.add_child(Panel::new(TextView::new(
-            parse_comment(&post.comment).replace("\n", "  "),
-        )));
-    }
-
-    Dialog::around(
-        OnEventView::new(layout.scrollable()).on_event(Key::Esc, |s| {
-            s.pop_layer();
-        }),
-    )
-    .title(format!("/{}/ #{}", &board, thread))
-    .button("Выйти", |s| s.quit())
 }
